a2c0209df7aaf432365b567332ff9541eaa36626
Divya Shetty <divyashettyk@gmail.com>
2021-05-08 18:08:31 -0500

    pie update

--- static/js/pieChart.js
+++ static/js/pieChart.js
@@ -34,18 +34,80 @@ function pieChart(seasonData){
 
     console.log("season data keys:",Object.keys(seasonData));
 
+    
+    // Create X title for seasons
+    seasonG = svg.append('g').append("text")
+                   .classed("aText season", true)
+                   .attr("transform", `translate(${width / 3}, ${height -2.5})`)
+                 
+                 //.attr("class", 'season')
+                 .text("Seasons");
+    monthG = svg.append('g').append("text")
+              .attr("transform", `translate(${width /2 + 60}, ${height -2.5})`)
+              .classed("aText month inactive inactive:hover", true)
+              .text("Months");   
+
     // set the color scale
     var color = d3.scaleOrdinal()
                   .domain(Object.keys(seasonData))
                   //.range(d3.schemeDark2);
                   .range(['#0275d8','#5cb85c','#d9534f','#f0ad4e'])
 
+    //Default pie chart
+    buildChartPie(color);
+                            
+    //Event- on click for months
+     //create a function to handle events
+     svg.selectAll(".aText").on("click", function() {
+
+            // get value of the selection
+            var value = d3.select(this).text();      
+            console.log(`Value of clicked title : ${value}`);
+
+            //set hover and active values
+            if(value === 'Months'){
+                                // set the color scale
+                var color = d3.scaleOrdinal()
+                            .domain(Object.keys(seasonData))
+                            .range(d3.schemeDark2);
+
+                //set style
+                d3.select(this).classed("inactive inactive:hover" , false)
+                seasonG.classed("inactive inactive:hover" , true)
+                
+
+                //call pie functon
+                buildChartPie(color)
+
+            }
+            else {
+                // set the color scale
+                var color = d3.scaleOrdinal()
+                              .domain(Object.keys(seasonData))
+                              .range(['#0275d8','#5cb85c','#d9534f','#f0ad4e'])
+
+                d3.select(this).classed("inactive inactive:hover" , false)
+                monthG.classed("inactive inactive:hover" , true)
+
+                //call the function to build pie chart
+                buildChartPie(color)
+            }
+
+              
+     });
+    
+
+
+
+function buildChartPie(color){
+    
+
                   //d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
 
     // Compute the position of each group on the pie:
     var pie = d3.pie()
                 .value(function(d) {return d.value; })
-                .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
+                //.sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
     
     // shape helper to build arcs:
     var arcGenerator = d3.arc()
@@ -82,17 +144,6 @@ function pieChart(seasonData){
                                  .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
                                  .attr('class','pie_text')
 
-    // Create X title for seasons
-    svg.append("text")
-              .attr("transform", `translate(${width / 3}, ${height -2.5})`)
-              .classed("aText season", true)
-              .text("Seasons");
-    svg.append("text")
-              .attr("transform", `translate(${width /2 + 60}, ${height -2.5})`)
-              .classed("aText month", true)
-              .text("Months");       
-              
-    
-
 }
 
+}
