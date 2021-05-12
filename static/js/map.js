685bbda0b22dac9ed029cb1a2ee1274a0ad46e2e
eukestad <69314608+eukestad@users.noreply.github.com>
2021-05-11 19:04:44 -0500

    adding map files

--- static/js/map.js
+++ static/js/map.js
@@ -1,45 +1,45 @@
+  
 var myMap = L.map("leaf")
-            .setView([-0.17578097424708533,15.732421875000002], 3);
+.setView([-0.17578097424708533,15.732421875000002], 3);
 
-streetLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        // minZoom: 5,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11",
-        accessToken: API_KEY
-}).addTo(myMap);
 
 satelliteLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        // minZoom: ,
-        zoomOffset: -1,
-        id: "mapbox/satellite-streets-v11",
-        accessToken: API_KEY
-});
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/satellite-streets-v11",
+accessToken: API_KEY
+}).addTo(myMap);
 
-var baseMaps = {
-        Streets: streetLayer,
-        Satellite: satelliteLayer
-};
+// var baseMaps = {
+// Streets: streetLayer,
+// Satellite: satelliteLayer
+// };
 
 function chooseColor(avg_temp) {
-if (avg_temp > 0) {
-        var color = "red";
-}
-else if (avg_temp < 0) {
-        var color = "blue";
-}
-else {
-        var color = "gray";
+    if (avg_temp > 120.958) {
+            var color = "red";
+    }
+    else if (avg_temp > 80.958) {
+            var color = "lightcoral";
+    }
+    else if (avg_temp > 40.958) {
+            var color = "royalblue";
+    }
+    else if (avg_temp > 0) {
+            var color = "darkblue";
+    }
+
+    else {
+            var color = "gray";
+    };
+
+    console.log(color);
+
+    return color;  
 };
 
-return color;  
-
-};
 var countriesGeo = 'https://opendata.arcgis.com/datasets/2b93b06dc0dc4e809d3c8db5cb96ba69_0.geojson'
 
 var climateUrl = `/launch_data`;
@@ -47,81 +47,116 @@ var climateUrl = `/launch_data`;
 // Grabbing our GeoJSON data..
 d3.json(countriesGeo).then(function(geoData) {
 
-        d3.json(climateUrl).then(function(climateData) {
+d3.json(climateUrl).then(function(climateData) {
 
-                // Creating a GeoJSON layer with the retrieved data
-                geoJsonLayer = L.geoJson(geoData, {
-                        // // Style each feature based on avg temp change
-                        style: {
-                        color: "darkgray",
-                        // fillColor: chooseColor(avg_temp),
-                        fillOpacity: 0.6,
-                        weight: 1
-                        },
-                        // Called on each feature
-                        onEachFeature: function(feature, layer) {
-                                var country = feature.properties.COUNTRY;
+    // Creating a GeoJSON layer with the retrieved data
+    countryLayer = L.geoJson(geoData, {
+            // // Style each feature based on avg temp change
+            style: {
+            color: "darkgray",
+            // fillColor: chooseColor(avg_temp),
+            fillOpacity: 0.6,
+            weight: 1
+            },
+            // Called on each feature
+            onEachFeature: function(feature, layer) {
+                    var country = feature.properties.COUNTRY;
 
-                                var countryInfo = climateData.filter(obj => country === obj.Country);
-                                console.log(countryInfo);
+                    var countryInfo = climateData.filter(obj => country === obj.Country);
+                    console.log(countryInfo[0]);
         
-                                if (typeof countryInfo !== 'undefined') {
-                                        feature.properties[avg_temp] = countryInfo['Avg Temp Change'];
-                                        feature.properties[avg_co2] = countryInfo['Avg Co2 Change'];
-                                        feature.properties[population] = countryInfo.Population;
-                                }
-                                else {
-                                        feature.properties[avg_temp] = '';
-                                        feature.properties[avg_co2] = '';
-                                        feature.properties[population] = '';
-                                };
-                             
-                                
-                                var avg_temp = feature.properties.avg_temp;
-                                var avg_co2 = feature.properties.avg_co2;
-                                var population = feature.properties.Population;
+                    if (countryInfo[0]) {
+
+                            console.log(countryInfo[0]['Avg Temp Change']);
+
+                            var avg_temp = countryInfo[0]['Avg Temp Change'];
+                            var avg_co2 = countryInfo[0]['Avg Co2 Change'];
+                            var population = countryInfo[0].Population;
+                            var flagLink = countryInfo[0].Images;
+                            
+                    }
+                    else {
+
+                            var avg_temp = 0;
+                            var avg_co2 = 0;
+                            var population = 0;
+                            var flagLink = '';
+                    };
+
+                    feature.properties.avg_temp = avg_temp;
+                    feature.properties.flag_Link = flagLink;
+                    console.log(feature.properties.avg_temp);
+            
+                    
+                    var popup = "<b>Country: </b>" + country + "<br><b>Avg Temp Change: </b>" + avg_temp + "<br><b>Avg CO2 Change: </b>" + avg_co2 + "<br><b>Population: </b>" + population;
+                    
+                    // Set mouse events to change map styling
+                    layer.on({
+                            // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+                            mouseover: function(event) {
+                                    layer = event.target;
+                                    layer.setStyle({
+                                            fillOpacity: 0.9,
+                                            weight: 5
+                                    });
+                                    this.openPopup()
+                            },
+                            // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+                            mouseout: function(event) {
+                                    layer = event.target;
+                                    layer.setStyle({
+                                            fillOpacity: 0.5,
+                                            weight: 1
+                                    });
+                                    this.closePopup()
+                            },
+                            // When a feature (country) is clicked, it is enlarged to fit the screen
+                            click: function(event) {
+
+                                    console.log('you clicked:', event.latlng);
+                           
+                                    countryDropdown = d3.select("#selDataset").node();
+                                    countryDropdown.value = country;
+
+                                    addDom(country);
+                                    demoInfo(country);
+                                    
+                            }
+                    });
+                    layer.setStyle({
+                            fillColor: chooseColor(feature.properties.avg_temp)
+                    });
+                    //   // Giving each feature a pop-up with information pertinent to it
+                    layer.bindPopup(popup);
+            }
+            }).addTo(myMap);
+
+        var legend = L.control({ position: "bottomright" });
+        legend.onAdd = function() {
+                var div = L.DomUtil.create("div", "legend");
         
-                                var popup = "<b>Country: </b>" + country + "<br><b>Avg Temp Change: </b>" + avg_temp + "<br><b>Avg CO2 Change: </b>" + avg_co2 + "<br><b>Population: </b>" + population;
-                                
-                                // Set mouse events to change map styling
-                                layer.on({
-                                        load: function(event) {
-                                                layer = event.target;
-                                                layer.setStyle({
-                                                        fillColor: chooseColor(avg_temp)
-                                                });
-                                        },
-                                        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-                                        mouseover: function(event) {
-                                                layer = event.target;
-                                                layer.setStyle({
-                                                        fillOpacity: 0.9,
-                                                        weight: 5
-                                        });
-                                        },
-                                        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-                                        mouseout: function(event) {
-                                                layer = event.target;
-                                                layer.setStyle({
-                                                        fillOpacity: 0.6,
-                                                        weight: 1
-                                        });
-                                        },
-                                        // When a feature (country) is clicked, it is enlarged to fit the screen
-                                        click: function(event) {
-                                                // console.log(event.latlng);
-                                                // // myMap.fitBounds(event.target.getBounds());
-                                                // myMap.setView(event.latlng,10)
-                                                
-                                        }
-                                });
-                                
-                                //   // Giving each feature a pop-up with information pertinent to it
-                                layer.bindPopup(popup);
-                        }
-                        }).addTo(myMap);
+                var colors = ['red', 'lightcoral', 'royalblue', 'darkblue','gray'];
+                var limits = ['120.959+', '80.959 to 120.958', '40.959 to 80.958', '0 to 40.958', 'No Data'];
+                var labels = [];
+            
+                // var legendInfo = "<p style='text-align:center;font-size:16'>Average Temperature Change</h5>";
+                var legendInfo = "<h6>Average Temperature Change</h6>";
+                
+                div.innerHTML = legendInfo;
+                
+                limits.forEach(function(limit, index) {
+                    labels.push("<li style=\"background-color: " + colors[index] + "\"></li> " + limits[index] + "<br>");
+                });
 
-        });
+                div.innerHTML += "<ul>" + labels.join(" ") + "</ul>";
+        
+                return div;
+        };
+        // Adding flag to the map
+        legend.addTo(myMap);
 
-  
 });
+
+
+});
+
---
+++ static/js/mimMap.js
@@ -0,0 +1,124 @@
+function buildMiniMap(country) {
+var myMap = L.map("country")
+.setView([0,0], 3);
+
+streetLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+// minZoom: 5,
+zoomOffset: -1,
+id: "mapbox/streets-v11",
+accessToken: API_KEY
+}).addTo(myMap);
+
+satelliteLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+// minZoom: ,
+zoomOffset: -1,
+id: "mapbox/satellite-streets-v11",
+accessToken: API_KEY
+});
+
+var baseMaps = {
+Streets: streetLayer,
+Satellite: satelliteLayer
+};
+
+var countriesGeo = 'https://opendata.arcgis.com/datasets/2b93b06dc0dc4e809d3c8db5cb96ba69_0.geojson'
+
+var climateUrl = `/launch_data/${country}`;
+
+// // Null variable that will hold layer
+// var countryLayer = L.geoJson(null, {style: {
+//         color: "orange",
+//         fillColor: "gray",
+//         fillOpacity: 0.6,
+//         weight: 3
+//         },
+// });
+
+// Grabbing our GeoJSON data..
+d3.json(countriesGeo).then(function(geoData) {
+        // countryLayer.addData(geoData, {filter: countryFilter});
+        var countryLayer = L.geoJson(geoData, 
+                {filter: countryFilter}, {style: {
+                color: "orange",
+                fillColor: "gray",
+                fillOpacity: 0.6,
+                weight: 3
+                },
+        }).addTo(myMap);
+
+        function countryFilter(feature) {
+                if (feature.properties.COUNTRY === country) return true
+              };
+
+        myMap.flyToBounds(countryLayer.getBounds())
+});
+
+// countryLayer.addTo(myMap);
+
+d3.json(climateUrl).then(function(climateData) {
+
+        var countryInfo = climateData.filter(obj => country === obj.Country);
+        console.log(countryInfo[0]);
+
+        if (countryInfo[0]) {
+
+                console.log(countryInfo[0]['Avg Temp Change']);
+
+                var avg_temp = countryInfo[0]['Avg Temp Change'];
+                var avg_co2 = countryInfo[0]['Avg Co2 Change'];
+                var population = countryInfo[0].Population;
+                var flagLink = countryInfo[0].Images;
+                var newCenter = [countryInfo[0].Lat, countryInfo[0].Lng];
+                
+        }
+        else {
+
+                var avg_temp = 0;
+                var avg_co2 = 0;
+                var population = 0;
+                var flagLink = '';
+                var newCenter = [0,0];
+        };
+
+        
+        var stats = L.control({position: "bottomright"});
+        stats.onAdd = function() {
+                var div = L.DomUtil.create("div", "stats");
+        
+                var popup = "<b>Avg Temp Change: </b>" + avg_temp + "<br><b>Avg CO2 Change: </b>" + avg_co2;
+        
+                div.innerHTML = popup;
+        
+                return div;
+        };
+        // Adding flag to the map
+        stats.addTo(myMap);
+
+        // return {popup: popup,
+        //         newcenter: newCenter};
+        var flag = L.control({ position: "bottomright" });
+        flag.onAdd = function() {
+                var div = L.DomUtil.create("div", "flag");
+        
+                var flagImg = "<img src=" + flagLink + " alt= 'flag of " + country + "' width='75' height='50'>";
+        
+                div.innerHTML = flagImg;
+        
+                return div;
+        };
+        // Adding flag to the map
+        flag.addTo(myMap);
+
+        // myMap.flyTo(newCenter);
+        
+});
+
+
+
+};
